5b00a1ae2852a6bdd35a4d82b1ec1c0902355648
shundroid <shundroid@users.noreply.github.com>
2016-07-03 20:15:51 +0900

    forEachを利用してblockCountを削除

--- js/block-manager.js
+++ js/block-manager.js
@@ -5,21 +5,20 @@ function BlockManager(editor) {
   if (typeof BlockManager.instance === "object") {
     return BlockManager.instance;
   }
-  const blockCount = 9;
   const builtInBlockCount = 3;
 
   this.editor = editor;
 
   this.blocks = [];
-  for (let i = 0; i < blockCount; i++) {
-    const blockElement = document.querySelector(`[data-block-index="${i}"]`);
-    this.blocks.push(new Block(i, blockElement, i < 3, this));
-  }
+  let blockElements = document.querySelectorAll("[data-block-index]");
+  Array.prototype.forEach.call(blockElements, (block, i) => {
+    this.blocks.push(new Block(i, block, i < builtInBlockCount, this));
+  });
 
   eventPublisher.subscribe("availableCommandsCount", (count) => {
     // 0-2番のblockはbuilt-in-command-button
-    for (let i = builtInBlockCount; i < blockCount; i++) {
-      this.blocks[i].setEnable(i < BlockManager + count);
+    for (let i = builtInBlockCount; i < blockElements.length; i++) {
+      this.blocks[i].setEnable(i < builtInBlockCount + count);
     }
   });
 
