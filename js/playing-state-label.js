088a8593dbfa4eae6845eb727845648177d6b325
shundroid <shundroid@users.noreply.github.com>
2016-07-05 16:55:36 +0900

    eslintに沿って修正

--- js/playing-state-label.js
+++ js/playing-state-label.js
@@ -6,7 +6,7 @@ function PlayingStateLabel(element) {
     if (gameState === "active") {
       this.element.textContent = "ゲーム中";
     } else {
-      this.element.textContent = "ゲームは開始されていません！"
+      this.element.textContent = "ゲームは開始されていません！";
     }
   });
 }
