c947a359a62ae9ea8140994be970f3e678cc2242
shundroid <shundroid@users.noreply.github.com>
2016-07-05 16:49:10 +0900

    added playing-state-label

---
+++ js/playing-state-label.js
@@ -0,0 +1,15 @@
+import eventPublisher from "./publisher";
+
+function PlayingStateLabel(element) {
+  this.element = element;
+  eventPublisher.subscribe("gameState", (gameState) => {
+    if (gameState === "active") {
+      this.element.textContent = "ゲーム中";
+    } else {
+      this.element.textContent = "ゲームは開始されていません！"
+    }
+  });
+}
+
+export default PlayingStateLabel;
+
