5e5910c8dbb01d96059431c676e4e38f0b2114e5
shundroid <shundroid@users.noreply.github.com>
2016-07-06 21:31:24 +0900

    joystickからいらなくなっていた部分を削除

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ import CalibrationButton from "./calibration-button";
 import HPBoard from "./hp-board";
 import GameStatusLabel from "./game-status-label";
 import InactiveScreen from "./inactive-screen";
-import SpheroStates from "./sphero-states";
 import SpheroClient from "./sphero-client";
 import ModeManager from "./mode-manager";
 import BlockManager from "./block-manager";
@@ -17,7 +16,6 @@ import "web-animations-js";
 document.addEventListener("DOMContentLoaded", function() {
   const editor = new Editor();
   new InactiveScreen();
-  new SpheroStates();
   new SpheroClient("ws://localhost:8080");
   new HPBoard(document.getElementById("hp"));
   new GameStatusLabel(document.getElementById("game-status-label"));
