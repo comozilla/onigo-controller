5530457e1a4ed8cb56b89f537e7bc7e31065bbd7
shundroid <shundroid@users.noreply.github.com>
2016-07-16 22:03:58 +0900

    save部分を実装

---
+++ js/backupSystem.js
@@ -0,0 +1,31 @@
+import eventPublisher from "./publisher";
+
+function BackupSystem() {
+  this.motions = {};
+  eventPublisher.subscribe("compile", args => {
+    this.motions[args.motion.motionId] = {
+      code: args.motion.motion.motionCode,
+      blockName: args.motion.motion.motionName
+    };
+    save.call(this);
+  });
+}
+
+BackupSystem.prototype.restore = function() {
+};
+
+function save() {
+  if (localStorage.length >= 10) {
+    const keys = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      if (!isNaN(key)) {
+        keys.push(parseInt(key));
+      }
+    }
+    localStorage.removeItem(keys.sort()[0]);
+  }
+  localStorage.setItem(Date.now(), this.motions);
+};
+
+module.exports = BackupSystem;
