d3bb08642aaa72c7af02532a7626c65009cbf8b5
shundroid <shundroid@users.noreply.github.com>
2016-07-26 20:32:08 +0900

    tryに入れるのを部分的に

--- js/parser.js
+++ js/parser.js
@@ -24,50 +24,50 @@ function Parser(logElement) {
   this.logElement = logElement;
   eventPublisher.subscribe("saveMotion", motion => {
     this.clear();
-    const rawFunction = Function.apply(
-      window,
-      [Object.keys(commandArgs),
-       Object.keys(availableFunctionArgs),
-       motion.motion.motionCode].reduce((a, b) => {
-      return a.concat(b);
-    }));
     const commands = [];
-    try {
-      const errors = [];
-      rawFunction.apply(window, Object.keys(commandArgs).map(commandName => {
-        return function() {
-          const validateResult = validateArgumentsType(arguments, commandArgs[commandName], true);
-          validateResult.errors.forEach(error => {
-            errors.push(error + " at " + commandName + "()");
-          });
-          if (commandArgs[commandName].length >= arguments.length) {
-            errors.push(`時間を指定してください。 at ${commandName}()`);
-          } else {
-            const time = arguments[commandArgs[commandName].length];
-            if (typeof time !== "number") {
-              errors.push(`時間が数値ではありません。 at ${commandName}()`);
-            } else if (time < 0.5) {
-              errors.push(`時間は0.5以上を使用してください。 at ${commandName}()`);
-            }
-            commands.push(new Command(commandName, validateResult.arrayArgs, time));
+    const errors = [];
+    const rawFunctionArgList = [Object.keys(commandArgs), Object.keys(availableFunctionArgs), motion.motion.motionCode].reduce((a, b) => {
+      return a.concat(b);
+    });
+    const rawFunctionArgs = Object.keys(commandArgs).map(commandName => {
+      return function() {
+        const validateResult = validateArgumentsType(arguments, commandArgs[commandName], true);
+        validateResult.errors.forEach(error => {
+          errors.push(error + " at " + commandName + "()");
+        });
+        if (commandArgs[commandName].length >= arguments.length) {
+          errors.push(`時間を指定してください。 at ${commandName}()`);
+        } else {
+          const time = arguments[commandArgs[commandName].length];
+          if (typeof time !== "number") {
+            errors.push(`時間が数値ではありません。 at ${commandName}()`);
+          } else if (time < 0.5) {
+            errors.push(`時間は0.5以上を使用してください。 at ${commandName}()`);
           }
-        }.bind(this);
-      }).concat(Object.keys(availableFunctionArgs).map(functionName => {
-        return function() {
-          const validateResult = validateArgumentsType(arguments, availableFunctionArgs[functionName], false);
-          validateResult.errors.forEach(error => {
-            errors.push(error + " at " + functionName + "()");
-          });
-          return new MotionSpecialData(functionName, validateResult.arrayArgs);
-        }.bind(this);
-      })));
-      if (errors.length > 0) {
-        throw new Error(errors.join("\n"));
-      }
+          commands.push(new Command(commandName, validateResult.arrayArgs, time));
+        }
+      }.bind(this);
+    }).concat(Object.keys(availableFunctionArgs).map(functionName => {
+      return function() {
+        const validateResult = validateArgumentsType(arguments, availableFunctionArgs[functionName], false);
+        validateResult.errors.forEach(error => {
+          errors.push(error + " at " + functionName + "()");
+        });
+        return new MotionSpecialData(functionName, validateResult.arrayArgs);
+      }.bind(this);
+    }));
+    try {
+      const rawFunction = Function.apply(window, rawFunctionArgList);
+      rawFunction.apply(window, rawFunctionArgs);
+    } catch (error) {
+      // SyntaxError、ReferenceErrorなどが入ってくる
+      errors.push(error.message);
+    }
+    if (errors.length > 0) {
+      this.log(errors.join("\n"), "error");
+    } else {
       eventPublisher.publish("compile", { motion, commands });
       this.log("コードのParseは正しく完了しました。", "success");
-    } catch (error) {
-      this.log(error.message, "error");
     }
   });
   instance = this;
